8fe07bef9a42d286dd087fc78ae79e9b89597bd7
Bob Ippolito <bob@redivi.com>
2024-06-23 19:26:27 -0700

    Fix selection

--- packages/lexical-builder-markdown/src/utils.ts
+++ packages/lexical-builder-markdown/src/utils.ts
@@ -65,22 +65,30 @@ export function $isEmptyParagraph(
  * the update will not change any content under the selection, but the
  * update you're running includes selection modifications that you do not
  * want.
+ *
+ * This is useful because the existing @lexical/markdown importers
+ * mangle the selection because of legacy mistakes.
  */
 export function $wrapWithIgnoreSelection<Parameters extends any[], Returns>(
   $update: (...args: Parameters) => Returns,
 ): (...args: Parameters) => Returns {
   return function wrappedWithIgnoreSelection(...args) {
     const prevSelection = $getSelection();
-    const clonedPrev = prevSelection ? prevSelection.clone() : null;
+    // We clone the selection because it could be mutated in-place
+    const savedSelection = prevSelection ? prevSelection.clone() : null;
     const rval = $update(...args);
     const nextSelection = $getSelection();
-    if (
-      // selection changed type
-      nextSelection !== prevSelection ||
-      // selection was mutated in-place
-      (clonedPrev && !clonedPrev.is(prevSelection))
-    ) {
-      $setSelection(clonedPrev);
+    // If it was not mutated in-place we restore it. If we had access to
+    // getActiveEditorState we could avoid using the clone and marking it
+    // as dirty
+    const restoredSelection =
+      savedSelection &&
+      savedSelection.is(prevSelection) &&
+      !Object.isFrozen(prevSelection)
+        ? prevSelection
+        : savedSelection;
+    if (nextSelection !== restoredSelection) {
+      $setSelection(restoredSelection);
     }
     return rval;
   };
