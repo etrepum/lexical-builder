a5312e2eebc0b768b88da62906db41fa4d0f7084
Bob Ippolito <bob@redivi.com>
2024-06-25 23:06:43 -0700

    remove unused test scripts

--- packages/lexical-tailwind/tests/unit/Tailwind.test.ts
+++
@@ -1,8 +0,0 @@
-import {} from "@etrepum/lexical-tailwind";
-import { describe, it, expect } from "vitest";
-
-describe("Tailwind", () => {
-  it("should have tests", () => {
-    expect(true).toBe(false);
-  });
-});
