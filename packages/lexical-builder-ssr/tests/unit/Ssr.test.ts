6b2464673d79b8e8c02fcfb661c735b2f7a56874
Bob Ippolito <bob@redivi.com>
2024-08-08 20:03:56 -0700

    template lexical-builder-ssr

---
+++ packages/lexical-builder-ssr/tests/unit/Ssr.test.ts
@@ -0,0 +1,8 @@
+import {} from "@etrepum/lexical-builder-ssr";
+import { describe, it, expect } from "vitest";
+
+describe("Ssr", () => {
+  it("should have tests", () => {
+    expect(true).toBe(false);
+  });
+});
